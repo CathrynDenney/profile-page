7c83455d0a46bea5d5b7abdb9560805a991efc44
Cathryn Denney <cdenney@Cathryns-MacBook-Pro-2.local>
2014-10-18 23:02:05 +0100

    created HTML structure

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en">
+
+<head>
+
+	<title>Cathryn Denney</title>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<meta name="description" content="" />
+
+	<link rel="icon" href="" />
+	<link rel="stylesheet" type="text/css" href="style.css" />
+
+	<!-- web fonts -->
+
+	<!-- / web fonts -->
+</head>
+
+<body>
+
+	<header>
+		<h1>Cathryn Denney</h1>
+		<nav>
+			<ul>
+				<li>Home</li>
+				<li>My Work</li>
+				<li>Contact</li>
+			</ul>
+		</nav>
+
+	</header>
+
+	<div class="main-content">
+		<img src="" />
+
+		<div id="about-cathryn">
+			<p>I'm a Digital Content Editor with Front-end dreams! <a href="www.cathryndenney.com/coursework.html">Working hard</a> for a Masters in Web Design &amp; Content Planning and immersing myself in the world of the web.</p>
+		</div>
+
+		<div class="section" id="skills">
+			<h2>Things I like to do</h2>
+				<ul>
+					<li>Code</li>
+					<li>Design</li>
+					<li>Content</li>
+					<li>Drink tea</li>
+				</ul>
+		</div>
+
+		<div class="section" id="supporting-info">
+			<h2>How I like to do them</h2>
+				<ul>
+					<li>HTML</li>
+					<li>CSS/Sass</li>
+					<li>jQuery</li>
+					<li>Git &amp; GitHub</li>
+					<li>RWD</li>
+					<li>Photoshop/Illustrator</li>
+				</ul>
+		</div>
+
+		<div class="section" id="blog">
+			<h2>Blog</h2>
+
+		</div>
+
+		<div class="section" id="contact">
+			<h2>Contact</h2>
+
+			<div class="location">
+				<h3>Physically you can find me here...</h3>
+		
+		
+				<p>London, UK</p>
+			</div>
+			<div class="online-links">
+				<h3>On the web you can find me here...</h3>
+
+				<ul>
+					<li>Twitter</li>
+					<li>GitHub</li>
+					<li>Lanyrd</li>
+					<li>LinkedIn</li>
+					<li>email</li>
+					<li>skype</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<footer>
+		&copy; Cathryn Denney <?php echo date("Y") ?>
+	</footer>
+
+</body>
+
+</html>
